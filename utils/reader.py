b4946d5e6303c64b626311a97086bb1fdbd134e2
AIwaifus <aiwaifusoy@proton.me>
2023-11-26 09:35:41 +0000

    Bump version: 1.2.2 → 1.2.3

--- utils/reader.py
+++ utils/reader.py
@@ -17,4 +17,31 @@ class Reader(object):
         print("Downloaded {} samples".format(len(data)))
 
         map_result = map(self._process_request, data)
-        self.data = [x[0] for x in ma
+        self.data = [x[0] for x in map_result]
+        self.lengths = [x[1] for x in map_result]
+        assert len(self.data) == len(self.lengths)
+
+    def _process_request(self, req):
+        """
+        Splits a request into lines and convert a string into ints.
+        """
+        seq = self.vocab.string_to_int(req)
+        l = len(seq)
+
+        return seq, l
+
+
+class Data(Reader):
+
+    def __init__(self, data_path, vocab=Vocabulary(), predict=False):
+        """
+        Creates an object that gets data from a file.
+        """
+        super(Data, self).__init__(data_path, vocab)
+
+        if not predict:
+            self._train_test_split()
+
+    def _train_test_split(self):
+        """
+        Train/val/test split for anom
