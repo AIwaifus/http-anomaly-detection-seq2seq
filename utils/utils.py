6d849c209e87c8cc3747d41ca10b63f31912d28f
AIwaifus <aiwaifusoy@proton.me>
2023-12-03 08:08:06 +0000

    build: Updating jakub-onderka/php-console-highlighter to 0.4.0

--- utils/utils.py
+++ utils/utils.py
@@ -31,4 +31,34 @@ def get_requests_from_file(path):
 
 
 def batch_generator(inputs, lengths, num_epochs, batch_size, vocab):
-   
+    """
+    Generates a padded batch. 
+    """
+    i = 0
+    input_size = len(inputs)
+    for _ in range(num_epochs):
+        while i + batch_size <= input_size:
+            l = lengths[i:i + batch_size]
+            padded = batch_padding(inputs[i:i + batch_size], l, vocab)
+            yield padded, l
+            i += batch_size
+        i = 0
+
+
+def one_by_one_generator(inputs, lengths, vocab):
+    """
+    Yields a sample. 
+    """
+    for i in range(len(inputs)):
+        yield [inputs[i]], lengths[i]
+
+
+def batch_padding(inputs, lengths, vocab):
+    """
+    Pads sequences to max sequence length in a batch.
+    """
+    max_len = np.max(lengths)
+    padded = []
+    for sample in inputs:
+        padded.append(
+            sample + ([vocab.vocab['<PAD>']] * (max_len - len(
